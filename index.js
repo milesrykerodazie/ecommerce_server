a104caed1e302ba06df0b437caabec94f808a478
Miles Ryker Odazie <rykerodazie@gmail.com>
2024-04-04 15:52:03 +0100

    update cors allowed domains

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ecommerce-front-end-test.vercel.app",
+    ],
     credentials: true,
   })
 );
